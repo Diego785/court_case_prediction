4549d94c242763e5861cc39fcc287cae8a982a07
Diego785 <diegohurtado2412@gmail.com>
2023-10-14 00:47:40 -0400

    uploading project

--- resources/views/my_views/dashboard/show_case.blade.php
+++ resources/views/my_views/dashboard/show_case.blade.php
@@ -6,3 +6,12 @@
 @section('sidebar')
     @include('my_views.layouts.sidebar')
 @endsection
+
+{{-- @section('navbar')
+    @include('my_views.layouts.navbar')
+@endsection --}}
+
+
+@section('content')
+    @livewire('caso', ['case_id' => $case->id])
+@endsection
